af05692cb2c7153b7bdb8512b098fee1da3de7e2
Destiny L <138726627+DestinyL321@users.noreply.github.com>
2023-11-07 16:11:37 -0500

    Update MessageAddRequest.cs

--- .NET/MessageAddRequest.cs
+++ .NET/MessageAddRequest.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Sabio.Models.Requests.Messages
+namespace WePair.Models.Requests.Messages
 {
     public class MessageAddRequest
     {
